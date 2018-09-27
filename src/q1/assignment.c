8f551a20220444f2eac29c66ef844d0d1723e118
ShaneOGrady97 <36577751+ShaneOGrady97@users.noreply.github.com>
2018-09-27 15:03:26 +0100

    question1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+int main(int arg, char* argc[]) {
+	int a = 0;
+	int* b = 0;
+	long c = 0;
+	double * d = 0;
+	char** e = 0;
+
+	printf("Shane O Grady 16357921 \n");
+	printf("int - %d \n", sizeof(a));
+	printf("int* - %d \n", sizeof(b));
+	printf("long - %d \n", sizeof(c));
+	printf("double * - %d \n", sizeof(d));
+	printf("char** - %d \n", sizeof(e));
+
+	system("pause");
+
 }
