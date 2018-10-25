598d466f20988ce19a42748fa25c55c76c3bf20e
ShaneOGrady97 <36577751+ShaneOGrady97@users.noreply.github.com>
2018-10-25 18:40:50 +0100

    Commit

--- src/q3/tests.h
+++ src/q3/tests.h
@@ -2,13 +2,6 @@
 #define CT331_ASSIGNMENT_TESTS
 
 
-
-void printChar(void *data);
-void printInt(void* data);
-void printStr(void* data);
-void printLong(void* data);
-void printFloat(void* data);
-
 void runTests();
 
 #endif
